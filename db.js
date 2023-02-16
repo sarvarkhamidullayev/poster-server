52aa7e80b1244ce2a7c5caab16f2feca2ae3535d
Sarvarbek <100616749+sarvarkhamidullayev@users.noreply.github.com>
2023-02-16 20:34:48 +0500

    first commit

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/test', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
